487caf98b4c1cca201f695352a4cab03b0597aff
michael-hawker <michael.hawker@outlook.com>
2018-07-05 18:25:09 -0700

    Push interactions with tool through editor. Add ability to load a file into the editor.

---
+++ Chakra Samples/JSRT Universal Windows Apps Hosting Samples/C#/JsRT_UWP_Sample/Actions/LoadFileAction.cs
@@ -0,0 +1,40 @@
+﻿using Monaco;
+using Monaco.Editor;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.Storage;
+using Windows.Storage.Pickers;
+
+namespace JsRT_UWP_Sample.Actions
+{
+    public class LoadFileAction : IActionDescriptor
+    {
+        public string ContextMenuGroupId => "navigation";
+        public float ContextMenuOrder => 1.5f;
+        public string Id => "meta-load-file-action";
+        public string KeybindingContext => null;
+        public int[] Keybindings => new int[] { Monaco.KeyMod.CtrlCmd | Monaco.KeyCode.KEY_L };
+        public string Label => "Load File...";
+        public string Precondition => null;
+
+        public async void Run(CodeEditor editor)
+        {
+            var ofd = new FileOpenPicker();
+            ofd.FileTypeFilter.Add(".js");
+
+            var file = await ofd.PickSingleFileAsync();
+
+            if (file != null)
+            {
+                var text = await FileIO.ReadTextAsync(file);
+
+                editor.Text = text;
+
+                editor.Focus(Windows.UI.Xaml.FocusState.Programmatic);
+            }
+        }
+    }
+}
--- Chakra Samples/JSRT Universal Windows Apps Hosting Samples/C#/JsRT_UWP_Sample/MainPage.xaml.cs
+++ Chakra Samples/JSRT Universal Windows Apps Hosting Samples/C#/JsRT_UWP_Sample/MainPage.xaml.cs
@@ -1,19 +1,7 @@
-﻿using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Runtime.InteropServices.WindowsRuntime;
-using Windows.Foundation;
-using Windows.Foundation.Collections;
+﻿using JsRT_UWP_Sample.Actions;
+using System;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
-using Windows.UI.Xaml.Controls.Primitives;
-using Windows.UI.Xaml.Data;
-using Windows.UI.Xaml.Input;
-using Windows.UI.Xaml.Media;
-using Windows.UI.Xaml.Navigation;
-
-// The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
 
 namespace JsRT_UWP_Sample
 {
@@ -24,12 +12,29 @@ namespace JsRT_UWP_Sample
         public MainPage()
         {
             this.InitializeComponent();
+
             string msg = host.init();
             if (msg != "NoError")
+            {
                 JsOutput.Text = msg;
+            }
+
+            JsInput.Text = @"// Use Ctrl+Enter to execute code
+// Press F1 to bring up commandbar or Ctrl+L to load a file.
+";
+            JsInput.Loaded += CodeEditor_Loaded;
         }
 
-        private void Button_Click(object sender, RoutedEventArgs e)
+        private async void CodeEditor_Loaded(object sender, RoutedEventArgs e)
+        {
+            await JsInput.AddCommandAsync(Monaco.KeyMod.CtrlCmd | Monaco.KeyCode.Enter, Execute_Code);
+
+            await JsInput.AddActionAsync(new LoadFileAction());
+            
+            JsInput.Focus(FocusState.Programmatic);
+        }
+
+        private void Execute_Code()
         {
             string result = host.runScript(JsInput.Text);
             JsOutput.Text = JsOutput.Text + "\n> " + JsInput.Text + "\n" + result;
